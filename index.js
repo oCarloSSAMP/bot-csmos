d97f79160f509bec0cd9242d3788191e39ca2ded
oCarloSSAMP <kakaraphael4@gmail.com>
2026-08-17 22:08:14 -0300

    gif atualizado]

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const config = require('./config.json');
 
 // 🖼️ GIFs do Bot
 const URL_GIF_ANUNCIO = 'https://i.imgur.com/JPErhA4.gif';
-const URL_GIF_MIX = 'https://i.imgur.com/7Vv0uXG.gif';
+const URL_GIF_MIX = 'https://i.imgur.com/JPErhA4.gif';
 
 const client = new Client({
     intents: [
